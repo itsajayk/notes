5f272fbc6dc8fc974f84077f9352b09b0cdaf5ba
itsajayk <itsajayk@outlook.com>
2024-06-16 23:04:50 +0530

    Removed unnecessary files

--- backend/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./notes-app-ajanthan-n-firebase-adminsdk-w7mko-d1f6ba0951.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
